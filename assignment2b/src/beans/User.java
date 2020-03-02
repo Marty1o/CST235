042bf1b84b2fd65b450c7dd2c49c7b67ee6c4778
Marty1o <52226629+Marty1o@users.noreply.github.com>
2020-03-01 18:52:17 -0700

    Update User.java

--- assignment2b/src/beans/User.java
+++ assignment2b/src/beans/User.java
@@ -2,11 +2,17 @@ package beans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @ManagedBean
 @ViewScoped
 public class User {
+	@NotNull
+	@Size(min = 5, max = 15)
 	private String firstName;
+	@NotNull
+	@Size(min = 5, max = 15)
 	private String lastName;
 
 	public User() {
