8fcd60e63ad1c70c3ebb0a6164fa942cb6380c21
Marty1o <52226629+Marty1o@users.noreply.github.com>
2020-03-08 05:00:58 -0700

    Update FormController.java

--- assignment2b/src/controllers/FormController.java
+++ assignment2b/src/controllers/FormController.java
@@ -1,14 +1,24 @@
 package controllers;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 import beans.User;
+import business.MyTimerService;
+import business.OrdersBusinessInterface;
 
 @ManagedBean
 @ViewScoped
 public class FormController {
+
+	@Inject
+	OrdersBusinessInterface services;
+
+	@EJB
+	MyTimerService timer;
 
 	public String onSubmit() {
 
@@ -18,13 +28,24 @@ public class FormController {
 		// this will get the values from the import form and pass them into User.class
 		User user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
 
-		// System.out.println("THE BUTTON WAS CLICKED");
+		// Will print out a message to the console which will tell us which business
+		// service is currently selected in the beans.xml file as an Alternative
+		services.test();
+
+		// star a timer when the login is clicked
+		timer.setTimer(5000);
 
 		// this will put the user object into the POST
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
 
 		// this will return the result into the TestForm page
 		return "TestResponse.xhtml";
+	}
+
+	public OrdersBusinessInterface getService() {
+
+		return services;
+
 	}
 
 	public String onFlash() {
@@ -35,7 +56,7 @@ public class FormController {
 		// this will get the values from the import form and pass them into User.class
 		User user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
 
-		//System.out.println("THE FLASH WAS CLICKED");
+		// System.out.println("THE FLASH WAS CLICKED");
 
 		// this will put the user object into the POST
 		FacesContext.getCurrentInstance().getExternalContext().getFlash().put("user", user);
